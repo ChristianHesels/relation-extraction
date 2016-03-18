ea153bb833029b0d5eade8d8f74ae6382dc89086
Tanja Bergmann <tabergma@gmail.com>
2016-03-18 16:55:07 +0100

    Write output of ReVerbExample to file.

--- core/src/main/java/edu/washington/cs/knowitall/examples/ReVerbExample.java
+++ core/src/main/java/edu/washington/cs/knowitall/examples/ReVerbExample.java
@@ -2,12 +2,18 @@ package edu.washington.cs.knowitall.examples;
 
 /* For representing a sentence that is annotated with pos tags and np chunks.*/
 
+import org.apache.commons.lang.StringUtils;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import edu.washington.cs.knowitall.extractor.ReVerbExtractor;
-import edu.washington.cs.knowitall.extractor.conf.ConfidenceFunction;
-import edu.washington.cs.knowitall.extractor.conf.ReVerbOpenNlpConfFunction;
 import edu.washington.cs.knowitall.nlp.ChunkedSentence;
+import edu.washington.cs.knowitall.nlp.ChunkedSentenceReader;
 import edu.washington.cs.knowitall.nlp.TreeTaggerSentenceChunker;
 import edu.washington.cs.knowitall.nlp.extraction.ChunkedBinaryExtraction;
+import edu.washington.cs.knowitall.util.DefaultObjects;
 
 /* String -> ChunkedSentence */
 /* The class that is responsible for extraction. */
@@ -19,17 +25,22 @@ import edu.washington.cs.knowitall.nlp.extraction.ChunkedBinaryExtraction;
 public class ReVerbExample {
 
     public static void main(String[] args) throws Exception {
+//        String sentence = "Die Audi AG mit Sitz in Ingolstadt in Bayern ist ein deutscher Automobilhersteller , der dem Volkswagen-Konzern angehört .";
+//        extractFromSentence(sentence);
 
-        String
-            sentStr =
-            "Die VW AG agiert als Muttergesellschaft der Fahrzeugmarken Volkswagen Pkw, Audi, Seat und Škoda.";
+        String fileName = "/home/tanja/Repositories/reverb/core/zeit.txt";
+        extractFromFile(fileName);
+    }
 
+
+    private static void extractFromSentence(String sentStr) throws IOException {
         // Looks on the classpath for the default model files.
         TreeTaggerSentenceChunker taggerSentenceChunker = new TreeTaggerSentenceChunker();
         ChunkedSentence sent = taggerSentenceChunker.chunkSentence(sentStr);
 
         // Prints out the (token, tag, chunk-tag) for the sentence
         System.out.println(sentStr);
+        System.out.println();
         for (int i = 0; i < sent.getLength(); i++) {
             String token = sent.getToken(i);
             String posTag = sent.getPosTag(i);
@@ -39,13 +50,55 @@ public class ReVerbExample {
 
         // Prints out extractions from the sentence.
         ReVerbExtractor reverb = new ReVerbExtractor(0, true, true, false);
-        ConfidenceFunction confFunc = new ReVerbOpenNlpConfFunction();
+//        ConfidenceFunction confFunc = new ReVerbOpenNlpConfFunction();
         for (ChunkedBinaryExtraction extr : reverb.extract(sent)) {
-            double conf = confFunc.getConf(extr);
+//            double conf = confFunc.getConf(extr);
+            System.out.println();
             System.out.println("Arg1=" + extr.getArgument1());
             System.out.println("Rel=" + extr.getRelation());
             System.out.println("Arg2=" + extr.getArgument2());
-            System.out.println("Conf=" + conf);
+//            System.out.println("Conf=" + conf);
         }
+    }
+
+    private static void extractFromFile(String fileName) throws IOException {
+        PrintWriter writer = new PrintWriter(fileName.replace(".txt", ".output.txt"));
+        ChunkedSentenceReader reader = new ChunkedSentenceReader(new FileReader(fileName), DefaultObjects.getDefaultSentenceExtractor());
+
+        System.out.println("Process sentences ...");
+        int n = 0;
+        for (ChunkedSentence s : reader.getSentences()) {
+            // Output progress
+            if (n % 10 == 0) {
+                System.out.print(n + " .. ");
+            }
+            n++;
+
+            // Prints out the (token, tag, chunk-tag) for the sentence
+            writer.println(s);
+            writer.println();
+            for (int i = 0; i < s.getLength(); i++) {
+                String token = s.getToken(i);
+                String posTag = s.getPosTag(i);
+                String chunkTag = s.getChunkTag(i);
+                writer.println(token + "\t" + posTag + "\t" + chunkTag);
+            }
+
+            // Prints out extractions from the sentence.
+            ReVerbExtractor reverb = new ReVerbExtractor(0, true, true, false);
+//            ConfidenceFunction confFunc = new ReVerbOpenNlpConfFunction();
+            for (ChunkedBinaryExtraction extr : reverb.extract(s)) {
+//                double conf = confFunc.getConf(extr);
+                writer.println();
+                writer.println("Arg1=" + extr.getArgument1());
+                writer.println("Rel=" + extr.getRelation());
+                writer.println("Arg2=" + extr.getArgument2());
+//                System.out.println("Conf=" + conf);
+            }
+            writer.println();
+            writer.println(StringUtils.repeat("-", 100));
+            writer.println();
+        }
+        System.out.println("Done.");
     }
 }
