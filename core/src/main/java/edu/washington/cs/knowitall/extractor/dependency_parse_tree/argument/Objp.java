e61d16f86294a65fa2cee96aca43a966bf33074b
Tanja Bergmann <tabergma@gmail.com>
2016-06-27 15:41:01 +0200

    Objp: Add prepostion also to complements.

--- core/src/main/java/edu/washington/cs/knowitall/extractor/dependency_parse_tree/argument/Objp.java
+++ core/src/main/java/edu/washington/cs/knowitall/extractor/dependency_parse_tree/argument/Objp.java
@@ -1,12 +1,12 @@
 package edu.washington.cs.knowitall.extractor.dependency_parse_tree.argument;
 
 
+import edu.washington.cs.knowitall.nlp.dependency_parse_tree.Node;
+import edu.washington.cs.knowitall.nlp.extraction.dependency_parse_tree.TreeExtraction;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import edu.washington.cs.knowitall.nlp.dependency_parse_tree.Node;
-import edu.washington.cs.knowitall.nlp.extraction.dependency_parse_tree.TreeExtraction;
 
 public class Objp extends Argument2 {
 
@@ -39,18 +39,23 @@ public class Objp extends Argument2 {
 
         // Add the main object
         List<Integer> ids = getIds(this.rootNode);
-        ids.add(0, this.preopsition.getId());
         extractions.add(new TreeExtraction(this.relation.getRootNode(), ids));
 
         // Add a extraction for each object in the conjunction
         extractions.addAll(resolveConjunction().stream()
                                .map(kon -> {
                                         List<Integer> konIds = getIds(kon);
-                                        konIds.add(0, this.preopsition.getId());
                                         return new TreeExtraction(this.relation.getRootNode(), konIds);
                                })
                                .collect(Collectors.toList()));
 
         return extractions;
+    }
+
+    @Override
+    protected List<Integer> getIds(Node n) {
+        List<Integer> ids = super.getIds(n);
+        ids.add(0, this.preopsition.getId());
+        return ids;
     }
 }
